e6269df9a7150025cb8c8bb8c25dba47fd6f2f46
Kyra Gunluk <kyra.gunluk@Kyras-Air.fios-router.home>
2019-05-11 18:14:31 -0400

    initial files

---
+++ SubSorterThenMerge_Sorter.java
@@ -0,0 +1,9 @@
+/**
+  Represent a merge operation for sorted lists,
+  as described in README.md
+ */
+import java.util.ArrayList;
+
+public class SubSortThenMerge_Sorter {
+
+    ArrayList<String> usersData;
